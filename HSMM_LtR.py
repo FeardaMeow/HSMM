e19fa731705c5fa76e805e2a09e70e741d7336e4
FeardaMeow <shwang1121@gmail.com>
2019-12-27 17:02:18 -0800

    Added dynamic transition matrix and duration estimate calculation
    Added two functions for calculating the dynamic transition matrix and estimating the state duration time

--- HSMM_LtR.py
+++ HSMM_LtR.py
@@ -20,11 +20,29 @@ class HSMM_LtR():
     def train(self):
         pass
 
-    def _calculate_transition_matrix(self):
-        pass
+    def _calculate_transition_matrix(self, d):
+        '''
+        Input:
+            d: Nx1 array of the current timestep duration estimates
+        Output:
+            N x N matrix of the current timesteps dynamic transition probabilities
+        '''
+        duration_probability = [(1 - self.f_duration.cdf(d[i], *self.duration_params[i]))/(1 - self.f_duration.cdf(d[i] - self.t_delta, *self.duration_params[i])) for i in range(self.N)]
+        self_transition_matrix = np.eye(self.N) * duration_probability 
+        return self_transition_matrix + (np.eye(self.N) - self_transition_matrix).dot(self.A)
 
     def _estimate_duration(self, d, A, alpha, obs_probs):
-        d_est = A.dot()
+        '''
+        Input:
+            d: Nx1 array of the previous timestep duration estimates
+            A: NxN matrix of the previous timestep dynamic transition matrix
+            alpha: 2xN matrix of the previous and current timestep forward probabilities
+            obs_probs: Nx1 array of the current timestep observation probabilities
+        Output:
+            N x 1 array for the estimated duration for the current timestep
+        '''
+        d_est = ((A.dot(alpha[0,:]).dot(obs_probs))/alpha[1,:])*(d+self.t_delta)
+        return d_est
 
     def _forward(self):
         pass
