2a3a1682fe37b36064273b4df4969ac4ac758e53
FeardaMeow <shwang1121@gmail.com>
2020-01-05 15:22:47 -0800

    Initial code for the forward probabilities

--- HSMM_LtR.py
+++ HSMM_LtR.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class HSMM_LtR():
-
+    # Log Likelihood = - sum_t( log ( 1 / forward norm scaling ) )
     def __init__(self, N, A=None, pi=None):
         self.N = N
         self.A = A
@@ -27,7 +27,7 @@ class HSMM_LtR():
         Output:
             N x N matrix of the current timesteps dynamic transition probabilities
         '''
-        duration_probability = [(1 - self.f_duration.cdf(d[i], *self.duration_params[i]))/(1 - self.f_duration.cdf(d[i] - self.t_delta, *self.duration_params[i])) for i in range(self.N)]
+        duration_probability = [(1 - self.f_duration.cdf(d[i], *self.duration_params[i]))/np.max(1 - self.f_duration.cdf(d[i] - self.t_delta, *self.duration_params[i]),0.0001) for i in range(self.N)]
         self_transition_matrix = np.eye(self.N) * duration_probability 
         return self_transition_matrix + (np.eye(self.N) - self_transition_matrix).dot(self.A)
 
@@ -44,8 +44,40 @@ class HSMM_LtR():
         d_est = ((A.dot(alpha[0,:]).dot(obs_probs))/alpha[1,:])*(d+self.t_delta)
         return d_est
 
-    def _forward(self):
-        pass
+    def _forward(self, obs_probs, ):
+        '''
+        Input:
+            obs_probs: TxN matrix of the observation probabilities at each timestep for all hidden states
+        Output:
+
+        '''
+        # Initialize the duration estimates
+        duration_est = np.ones(obs_probs.shape)*self.t_delta
+
+        # Initialize the dynamic transition matrix
+        A_dt = np.zeros((self.N, self.N, obs_probs.shape[0]))
+        A_dt[:,:,0] = self._calculate_transition_matrix(duration_est[0,:])
+
+        # Initialize the forward probabilities
+        alpha = np.zeros(obs_probs.shape)
+        alpha[0,:] = self.pi*obs_probs[0,:]
+
+        log_likelihood = np.log(1/np.sum(alpha[0,:]))
+
+        alpha[0,:] /= np.sum(alpha[0,:])
+        # Log Likelihood = - sum_t( log ( 1 / forward norm scaling ) )
+
+        for t in range(1,obs_probs.shape[0]):
+            alpha[t,:] = alpha[t-1,:].dot(A_dt[:,:,t-1])*obs_probs[t,:]
+            log_likelihood += np.log(1/np.sum(alpha[t,:]))
+            alpha[t,:] /= np.sum(alpha[t,:])
+            # Estimate next duration
+            duration_est[t,:] = self._estimate_duration(duration_est[t-1,:], A_dt[:,:,t-1], alpha[t-1:t+1,:], obs_probs[t,:])
+            A_dt[:,:,t] = self._calculate_transition_matrix(duration_est[t,:])
+        
+        log_likelihood *= -1
+        
+        return(alpha, A_dt, duration_est, log_likelihood)
 
     def _backward(self):
         pass
